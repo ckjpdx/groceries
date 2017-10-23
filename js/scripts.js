bb9f0f63b9e91c09663bfbe01c6a3d1a30ad5314
Chris Johnson and Luke Bertram <luke@>
2017-10-23 14:39:21 -0700

    Add uppercasing, alphabetizing to displayed grocery list

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,17 @@ $(document).ready(function(){
   $("#groceryForm").submit(function(event){
     event.preventDefault();
     var arrGroceries = [];
-    for (var i = 0; i < 6; i++) { // add variable length
+    for (var i = 0; i < $('.grocItemClass').length; i++) { // add variable length
       var grocery = "#groc" + (i + 1);
       if ($(grocery).val()) {
-        arrGroceries[i] = $(grocery).val();
+        arrGroceries.push($(grocery).val());
       }
     }
     arrGroceries = arrGroceries.sort();
-    debugger;
+    arrGroceries = arrGroceries.map(function(grocery){
+      grocery = grocery.toUpperCase();
+      return grocery;
+    });
     arrGroceries.forEach(function(item){
       $("#groceryUl").append('<li>' + item + '</li>');
     });
